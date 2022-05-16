1c997273c9c29f6d7490f2672ae1ab25905bddbf
svetilnikovdb <danila.svetilnikov@mail.ru>
2022-05-16 18:20:42 +0300

    From JSON. Thanks, Nikita

--- Station.py
+++ Station.py
@@ -1,8 +1,12 @@
 from sympy import Point
+import json
 
 
 class Station:
     position: Point
 
-    def __init__(self, point: Point):
-        self.position = point
+    def __init__(self, *args):
+        if isinstance(args[0], Point):
+            self.position = args[0]
+        elif isinstance(args[0], dict):
+            self.position = Point(args[0]["station"]["x"], args[0]["station"]["y"])
