a7cc9d2ac41320779fe25838f730c05bdc258c92
Nikita Utkin <n.utkin@headsandhands.ru>
2022-06-07 14:26:28 +0300

    Робот едет до дока

--- Strategy.py
+++ Strategy.py
@@ -12,11 +12,12 @@ RIGHT_ROTATE = math.pi / 180
 
 class Strategy:
     robot: Robot
+    forward_cnt: int = 0
     station: Station
     barriers: [Barrier]
     touchWall: bool = False
     rotateCnt: int = 0
-    lastRotate: int = 0
+    prevRotate: int = 0
 
     def __init__(self, robot: Robot, station: Station, barriers: [Barrier]):
         self.robot = robot
@@ -25,11 +26,13 @@ class Strategy:
         self.barriers = barriers
 
     def update(self):
+
         if self.robot.on_station(self.station):
             return
 
         if self.touchWall:
             self.goTouchingWall()
+            self.forward_cnt = 0
             return
 
         # if self.rotateCnt > 10:
@@ -37,61 +40,72 @@ class Strategy:
         #     self.rotateCnt = 0
         #     return
 
-        view = self.robot.check_front(self.barriers)
+        view_barriers_distances = self.robot.check_front(self.barriers)
 
         # if min(view) > self.robot.radius and self.lastRotate == 10:
         #     self.robot.rotate(self.robot.get_angle_to_station(self.station))
         #     return
 
-        if min(view) > self.robot.radius:
+        if (self.forward_cnt > self.robot.radius * 2 + 1) and abs(self.robot.get_angle_to_station(self.station)) > (1e-10):
+            rotate_angle = self.robot.get_angle_to_station(self.station)
+            print(rotate_angle * 180 / math.pi)
+            if rotate_angle > 0:
+                rotate_angle = min(rotate_angle, math.pi / 180 * 90)
+            else:
+                rotate_angle = max(rotate_angle, math.pi / 180 * 90)
+            self.robot.rotate(rotate_angle)
+
+        if min(view_barriers_distances) > self.robot.radius:
             self.robot.forward(FORWARD_STEP)
+            #self.robot.rotate(self.robot.get_angle_to_station(self.station))
             self.rotateCnt = 0
+            self.forward_cnt += 1
             return
 
         else:
             self.touchWall = True
-            self.lastRotate = 0
-            self.robot.rotate(self.robot.get_angle_to_station(self.station))
+            self.prevRotate = 0
             self.rotateCnt += 1
             return
 
     def update_and_get_state(self):
         start_time = time.time()
         self.update()
-        print(time.time() - start_time)
+        # print(time.time() - start_time)
         return Robot.RobotState(self.robot, self.station)
 
     def goTouchingWall(self):
-        view = self.robot.check_front(self.barriers)
+        view_barriers_distances = self.robot.check_front(self.barriers)
 
-        if min(view) > self.robot.view_distance and self.lastRotate == 0:
+        if min(view_barriers_distances) > self.robot.view_distance and self.prevRotate == 0:
             self.touchWall = False
-            self.lastRotate = 10
+            self.prevRotate = 0
             return
 
-        if self.robot.radius < min(view):
+        if self.robot.radius < min(view_barriers_distances):
             self.robot.forward(FORWARD_STEP)
             self.rotateCnt = 0
-            self.lastRotate = 0
+            self.prevRotate = 0
             return
 
-        if self.lastRotate != 0:
-            self.robot.rotate(LEFT_ROTATE if self.lastRotate == -1 else RIGHT_ROTATE)
+        if self.prevRotate != 0:
+            rotate_angle = LEFT_ROTATE if self.prevRotate == -1 else RIGHT_ROTATE
+            self.robot.rotate(rotate_angle)
             return
 
-        if view[0] > self.robot.view_distance and view[2] > self.robot.view_distance:
+        if view_barriers_distances[0] > self.robot.view_distance and view_barriers_distances[2] > self.robot.view_distance:
             self.robot.rotate(LEFT_ROTATE)
-            self.lastRotate = -1
+            self.prevRotate = -1
             return
         # if self.rotateCnt > 30:
         #     self.rotateCnt = 0
         #     self.touchWall = False
         #     return
 
-        if view[0] <= view[2]:
+        if view_barriers_distances[0] <= view_barriers_distances[2]:
             self.rotateCnt += 1
             self.robot.rotate(RIGHT_ROTATE)
-            self.lastRotate = 1
+            self.prevRotate = 1
         else:
             self.robot.rotate(LEFT_ROTATE)
-            self.lastRotate = -1
+            self.prevRotate = -1
